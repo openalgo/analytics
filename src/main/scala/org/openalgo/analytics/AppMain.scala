3b67f38a1193ebcef1cd138bbdd5085a79126002
Daniel Yinanc <daniel.yinanc@ericsson.com>
2016-03-19 16:11:59 -0400

    Analytics server now starts on port 9002 and is configurable

--- src/main/scala/org/openalgo/analytics/AppMain.scala
+++ src/main/scala/org/openalgo/analytics/AppMain.scala
@@ -6,14 +6,17 @@ import akka.http.scaladsl.model.StatusCodes.OK
 import akka.http.scaladsl.server.Directives.{as, complete, decodeRequest, entity, path, post}
 import akka.stream.ActorMaterializer
 import argonaut.Argonaut.casecodec2
+import com.typesafe.config.ConfigFactory
 import de.heikoseeberger.akkahttpargonaut.ArgonautSupport
-import scala.collection.JavaConverters._
 
+import scala.collection.JavaConverters._
 import scala.collection.mutable
 
 case class CovarianceInput(series1: mutable.Buffer[java.lang.Double], series2: mutable.Buffer[java.lang.Double])
 
 object AppMain extends App with ArgonautSupport {
+  val config = ConfigFactory.load()
+
   implicit def PersonCodecJson =
     casecodec2(CovarianceInput.apply, CovarianceInput.unapply)("series1", "series2")
 
@@ -32,7 +35,7 @@ object AppMain extends App with ArgonautSupport {
       }
     }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val bindingFuture = Http().bindAndHandle(route, config.getString("http.interface"), config.getInt("http.port"))
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
   Console.readLine() // for the future transformations
